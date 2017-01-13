750b7f47f4ae151b8e650aafb51a3c2d16e34fbc
carlgarpel <carlgarpel@gmail.com>
2017-01-14 00:46:06 +0100

    Add files via upload

---
+++ www/js/camera.js
@@ -0,0 +1,89 @@
+var app = {
+	inicio: function() {
+		this.iniciaFastClick();
+		this.iniciaBotones();
+	},
+
+	iniciaFastClick: function () {
+		FastClick.attach (document.body);
+	},
+
+
+	 iniciaBotones: function() {
+    var buttonAction = document.querySelector('#button-action');
+    buttonAction.addEventListener('click', this.tomarFoto);
+
+    var filterButtons = document.querySelectorAll('.button-filter');
+    filterButtons[0].addEventListener('click', function(){
+      app.aplicaFiltro('gray');
+    });
+    filterButtons[1].addEventListener('click', function(){
+      app.aplicaFiltro('negative');
+    });
+    filterButtons[2].addEventListener('click', function(){
+      app.aplicaFiltro('sepia');
+    });
+
+   
+  },
+
+	tomarFoto: function() {
+	var opciones = {
+		quality:100,
+		destinationType: Camera.DestinationType.FILE_URI,
+		targetWidth: 300,
+		targetHeight: 300,
+		correctOrientation: true
+	};
+		navigator.camera.getPicture(app.fotoTomada, app.errorAlTomarFoto, opciones);
+	},
+
+	fotoTomada: function(imageURI) {
+		var img = document.createElement('img');
+
+		img.onload = function() {
+			app.pintarFoto(img);
+		};
+
+		img.src = imageURI;
+	
+
+		
+	},
+
+	pintarFoto: function (img) {
+	
+
+		var c = document.querySelector('#foto');
+		var ctex = c.getContext('2d');
+		c.height = img.height;
+		c.width = img.width;
+		ctex.drawImage(img, 0, 0, img.width, img.height);
+			var salida= document.querySelector('.header');
+			salida.innerHTML ="Carlos: hombre joven de gran atractivo";
+
+	},
+
+	errorAlTomarFoto: function(message){
+	console.log ("Fallo al tomar foto o toma cancelada: " + message);
+	},
+
+ 	aplicaFiltro: function(filterName) {
+    	var canvas = document.querySelector('#foto');
+    	var context = canvas.getContext('2d');
+    	imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+    	effects[filterName](imageData.data);
+
+    	context.putImageData(imageData, 0, 0);
+  }
+};
+
+var imageData;
+
+if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		app.inicio();
+	}, false);
+}
+
